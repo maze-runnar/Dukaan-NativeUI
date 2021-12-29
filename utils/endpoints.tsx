b514f9cc6286df622f6fb2c8110e0bbd679688ed
Dubey <sundaram.dubey@SW-LP12481.local>
2021-12-29 11:19:32 +0530

    component path updated

--- utils/endpoints.tsx
+++ utils/endpoints.tsx
@@ -2,10 +2,10 @@ const ENDPOINTS = {
     LOGIN: "/api/v1/user/login",
     REGISTER: "/api/v1/user/auth",
     USER_DETAIL: "/api/v1/user/", // takes userid <userid>
-    NEW_NOTIFICATION_ADD: "/api/v1/notification/new/", 
-    NOTIFICATION_LIST:"/api/v1/notification/", //<merchantid>
+    NEW_NOTIFICATION_ADD: "/api/v1/notification/new/",
+    NOTIFICATION_LIST: "/api/v1/notification/", //<merchantid>
     NEAR_BY_SHOPS: "/api/v1/shop/nearby/", //<userid>
-    NEW_SHOP: "/api/v1/merchant/shop/new", 
+    NEW_SHOP: "/api/v1/merchant/shop/new",
     SHOP_DETAIL: "/api/v1/shop/", //<shopid> complete api /api/v1/shop/shopid
     SHOP_LIST: "/api/v1/merchant/shops/", //<merchantid>
     MERCHANT_LOGIN: "/api/v1/merchant/login",
@@ -14,6 +14,10 @@ const ENDPOINTS = {
     NEW_ITEM: "/api/v1/shop/item/new",
     ITEM_LIST: "/api/v1/shop/items/", //<shopid>
     ITEM_DETAIL: "/api/v1/item/", //<itemid>
+    UPLOAD_IMAGE: "/api/v1/upload/image",
+    NEW_KHATA_RECORD: "/api/v1/khata/new",
+    KHATA_LIST: "/api/v1/shop/khata/", // <shopid> /api/v1/shop/khata/<shopid>
+    KHATA_DETAIL: "/api/v1/khata/" //<khataid>  /api/v1/khata/<khataid>
 };
 
 export default ENDPOINTS;
